1c2337afee9a61c3190edc8bb8de26f057c51b20
Shyam <shyam@nimbleaiservices.com>
2025-09-05 01:27:28 -0500

    feat: vcard base

---
+++ wordpress/wp-content/plugins/digi-vcard/digi-vcard.php
@@ -0,0 +1,386 @@
+<?php
+/**
+ * Plugin Name: Digi vCard
+ * Plugin URI: https://your-website.com
+ * Description: Create professional digital business cards with multiple templates and custom fields.
+ * Version: 1.0.0
+ * Author: Your Name
+ * License: GPL v2 or later
+ * Text Domain: digi-vcard
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Define plugin constants
+define('DIGI_VCARD_VERSION', '1.0.0');
+define('DIGI_VCARD_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('DIGI_VCARD_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+/**
+ * Main Plugin Class
+ */
+class DigiVCard {
+    
+    public function __construct() {
+        add_action('init', array($this, 'init'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+        register_activation_hook(__FILE__, array($this, 'activate'));
+        register_deactivation_hook(__FILE__, array($this, 'deactivate'));
+    }
+    
+    public function init() {
+        $this->register_post_type();
+        $this->register_taxonomies();
+        $this->add_admin_hooks();
+        $this->add_template_hooks();
+    }
+    
+    /**
+     * Register vCard Custom Post Type
+     */
+    public function register_post_type() {
+        $labels = array(
+            'name'                  => _x('vCards', 'Post type general name', 'digi-vcard'),
+            'singular_name'         => _x('vCard', 'Post type singular name', 'digi-vcard'),
+            'menu_name'             => _x('vCards', 'Admin Menu text', 'digi-vcard'),
+            'name_admin_bar'        => _x('vCard', 'Add New on Toolbar', 'digi-vcard'),
+            'add_new'               => __('Add New', 'digi-vcard'),
+            'add_new_item'          => __('Add New vCard', 'digi-vcard'),
+            'new_item'              => __('New vCard', 'digi-vcard'),
+            'edit_item'             => __('Edit vCard', 'digi-vcard'),
+            'view_item'             => __('View vCard', 'digi-vcard'),
+            'all_items'             => __('All vCards', 'digi-vcard'),
+            'search_items'          => __('Search vCards', 'digi-vcard'),
+            'not_found'             => __('No vCards found.', 'digi-vcard'),
+            'not_found_in_trash'    => __('No vCards found in Trash.', 'digi-vcard'),
+        );
+
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'show_in_rest'       => true,
+            'query_var'          => true,
+            'rewrite'            => array('slug' => 'vcard'),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => 20,
+            'menu_icon'          => 'dashicons-id-alt',
+            'supports'           => array('title', 'editor', 'thumbnail', 'excerpt'),
+        );
+
+        register_post_type('vcard', $args);
+    }
+    
+    /**
+     * Register Taxonomies
+     */
+    public function register_taxonomies() {
+        // vCard Industries
+        register_taxonomy('vcard_industry', array('vcard'), array(
+            'hierarchical'      => true,
+            'labels'            => array(
+                'name'              => _x('Industries', 'taxonomy general name', 'digi-vcard'),
+                'singular_name'     => _x('Industry', 'taxonomy singular name', 'digi-vcard'),
+                'menu_name'         => __('Industries', 'digi-vcard'),
+                'all_items'         => __('All Industries', 'digi-vcard'),
+                'edit_item'         => __('Edit Industry', 'digi-vcard'),
+                'update_item'       => __('Update Industry', 'digi-vcard'),
+                'add_new_item'      => __('Add New Industry', 'digi-vcard'),
+                'new_item_name'     => __('New Industry Name', 'digi-vcard'),
+            ),
+            'show_ui'           => true,
+            'show_in_rest'      => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => array('slug' => 'vcard-industry'),
+        ));
+    }
+    
+    /**
+     * Add Admin Hooks
+     */
+    public function add_admin_hooks() {
+        add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+        add_action('save_post', array($this, 'save_meta_boxes'));
+        add_filter('manage_vcard_posts_columns', array($this, 'admin_columns'));
+        add_action('manage_vcard_posts_custom_column', array($this, 'admin_column_content'), 10, 2);
+    }
+    
+    /**
+     * Add Template Hooks
+     */
+    public function add_template_hooks() {
+        add_filter('single_template', array($this, 'load_single_template'));
+        add_filter('archive_template', array($this, 'load_archive_template'));
+        add_filter('the_content', array($this, 'display_vcard_content'));
+    }
+    
+    /**
+     * Load Single vCard Template
+     */
+    public function load_single_template($template) {
+        if (is_singular('vcard')) {
+            $plugin_template = DIGI_VCARD_PLUGIN_PATH . 'templates/single-vcard-page.php';
+            if (file_exists($plugin_template)) {
+                return $plugin_template;
+            }
+        }
+        return $template;
+    }
+    
+    /**
+     * Load Archive vCard Template
+     */
+    public function load_archive_template($template) {
+        if (is_post_type_archive('vcard')) {
+            $plugin_template = DIGI_VCARD_PLUGIN_PATH . 'templates/archive-vcard.php';
+            if (file_exists($plugin_template)) {
+                return $plugin_template;
+            }
+        }
+        return $template;
+    }
+    
+    /**
+     * Display vCard Content
+     */
+    public function display_vcard_content($content) {
+        if (is_singular('vcard') && is_main_query() && !is_admin()) {
+            ob_start();
+            include DIGI_VCARD_PLUGIN_PATH . 'templates/single-vcard.php';
+            $vcard_content = ob_get_clean();
+            return $vcard_content;
+        }
+        return $content;
+    }
+    
+    /**
+     * Add Meta Boxes
+     */
+    public function add_meta_boxes() {
+        add_meta_box(
+            'vcard_basic_info',
+            __('Basic Information', 'digi-vcard'),
+            array($this, 'basic_info_meta_box'),
+            'vcard',
+            'normal',
+            'high'
+        );
+        
+        add_meta_box(
+            'vcard_contact_info',
+            __('Contact Information', 'digi-vcard'),
+            array($this, 'contact_info_meta_box'),
+            'vcard',
+            'normal',
+            'high'
+        );
+    }
+    
+    /**
+     * Basic Info Meta Box
+     */
+    public function basic_info_meta_box($post) {
+        wp_nonce_field('vcard_meta_box', 'vcard_meta_box_nonce');
+        
+        $job_title = get_post_meta($post->ID, '_vcard_job_title', true);
+        $company = get_post_meta($post->ID, '_vcard_company', true);
+        
+        echo '<table class="form-table">';
+        echo '<tr>';
+        echo '<th><label for="vcard_job_title">' . __('Job Title', 'digi-vcard') . '</label></th>';
+        echo '<td><input type="text" id="vcard_job_title" name="vcard_job_title" value="' . esc_attr($job_title) . '" class="regular-text" /></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<th><label for="vcard_company">' . __('Company/Organization', 'digi-vcard') . '</label></th>';
+        echo '<td><input type="text" id="vcard_company" name="vcard_company" value="' . esc_attr($company) . '" class="regular-text" /></td>';
+        echo '</tr>';
+        echo '</table>';
+    }
+    
+    /**
+     * Contact Info Meta Box
+     */
+    public function contact_info_meta_box($post) {
+        $email = get_post_meta($post->ID, '_vcard_email', true);
+        $phone = get_post_meta($post->ID, '_vcard_phone', true);
+        $office_phone = get_post_meta($post->ID, '_vcard_office_phone', true);
+        $address = get_post_meta($post->ID, '_vcard_address', true);
+        
+        echo '<table class="form-table">';
+        echo '<tr>';
+        echo '<th><label for="vcard_email">' . __('Email Address', 'digi-vcard') . '</label></th>';
+        echo '<td><input type="email" id="vcard_email" name="vcard_email" value="' . esc_attr($email) . '" class="regular-text" /></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<th><label for="vcard_phone">' . __('Phone Number', 'digi-vcard') . '</label></th>';
+        echo '<td><input type="text" id="vcard_phone" name="vcard_phone" value="' . esc_attr($phone) . '" class="regular-text" /></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<th><label for="vcard_office_phone">' . __('Office Phone', 'digi-vcard') . '</label></th>';
+        echo '<td><input type="text" id="vcard_office_phone" name="vcard_office_phone" value="' . esc_attr($office_phone) . '" class="regular-text" /></td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<th><label for="vcard_address">' . __('Address', 'digi-vcard') . '</label></th>';
+        echo '<td><textarea id="vcard_address" name="vcard_address" rows="3" class="large-text">' . esc_textarea($address) . '</textarea></td>';
+        echo '</tr>';
+        echo '</table>';
+    }
+    
+    /**
+     * Save Meta Boxes
+     */
+    public function save_meta_boxes($post_id) {
+        if (!isset($_POST['vcard_meta_box_nonce']) || !wp_verify_nonce($_POST['vcard_meta_box_nonce'], 'vcard_meta_box')) {
+            return;
+        }
+        
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+        
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+        
+        // Save basic info
+        if (isset($_POST['vcard_job_title'])) {
+            update_post_meta($post_id, '_vcard_job_title', sanitize_text_field($_POST['vcard_job_title']));
+        }
+        
+        if (isset($_POST['vcard_company'])) {
+            update_post_meta($post_id, '_vcard_company', sanitize_text_field($_POST['vcard_company']));
+        }
+        
+        // Save contact info
+        if (isset($_POST['vcard_email'])) {
+            update_post_meta($post_id, '_vcard_email', sanitize_email($_POST['vcard_email']));
+        }
+        
+        if (isset($_POST['vcard_phone'])) {
+            update_post_meta($post_id, '_vcard_phone', sanitize_text_field($_POST['vcard_phone']));
+        }
+        
+        if (isset($_POST['vcard_office_phone'])) {
+            update_post_meta($post_id, '_vcard_office_phone', sanitize_text_field($_POST['vcard_office_phone']));
+        }
+        
+        if (isset($_POST['vcard_address'])) {
+            update_post_meta($post_id, '_vcard_address', sanitize_textarea_field($_POST['vcard_address']));
+        }
+    }
+    
+    /**
+     * Admin Columns
+     */
+    public function admin_columns($columns) {
+        $new_columns = array();
+        $new_columns['cb'] = $columns['cb'];
+        $new_columns['title'] = $columns['title'];
+        $new_columns['job_title'] = __('Job Title', 'digi-vcard');
+        $new_columns['email'] = __('Email', 'digi-vcard');
+        $new_columns['phone'] = __('Phone', 'digi-vcard');
+        $new_columns['date'] = $columns['date'];
+        
+        return $new_columns;
+    }
+    
+    /**
+     * Admin Column Content
+     */
+    public function admin_column_content($column, $post_id) {
+        switch ($column) {
+            case 'job_title':
+                echo esc_html(get_post_meta($post_id, '_vcard_job_title', true));
+                break;
+            case 'email':
+                echo esc_html(get_post_meta($post_id, '_vcard_email', true));
+                break;
+            case 'phone':
+                echo esc_html(get_post_meta($post_id, '_vcard_phone', true));
+                break;
+        }
+    }
+    
+    /**
+     * Enqueue Scripts and Styles
+     */
+    public function enqueue_scripts() {
+        if (is_singular('vcard') || is_post_type_archive('vcard')) {
+            wp_enqueue_style('digi-vcard-style', DIGI_VCARD_PLUGIN_URL . 'assets/style.css', array(), DIGI_VCARD_VERSION);
+            wp_enqueue_script('digi-vcard-script', DIGI_VCARD_PLUGIN_URL . 'assets/vcard.js', array(), DIGI_VCARD_VERSION, true);
+        }
+    }
+    
+    /**
+     * Plugin Activation
+     */
+    public function activate() {
+        $this->register_post_type();
+        $this->register_taxonomies();
+        flush_rewrite_rules();
+        
+        // Add default categories
+        $categories = array(
+            'CEO' => 'Chief Executive Officer and Business Leaders',
+            'Construction' => 'Construction and Building Industry',
+            'Coffeebar' => 'Coffee Shop and Restaurant Industry',
+            'Healthcare' => 'Medical and Healthcare Professionals',
+            'Education' => 'Teachers and Educational Professionals',
+            'Freelancer' => 'Freelancers and Independent Contractors',
+        );
+        
+        foreach ($categories as $name => $description) {
+            if (!term_exists($name, 'vcard_industry')) {
+                wp_insert_term($name, 'vcard_industry', array('description' => $description));
+            }
+        }
+    }
+    
+    /**
+     * Plugin Deactivation
+     */
+    public function deactivate() {
+        flush_rewrite_rules();
+    }
+}
+
+// Initialize the plugin
+new DigiVCard();
+
+/**
+ * Helper Functions
+ */
+function get_vcard_meta($post_id, $key) {
+    return get_post_meta($post_id, '_vcard_' . $key, true);
+}
+
+function display_vcard($post_id = null) {
+    if (!$post_id) {
+        global $post;
+        $post_id = $post->ID;
+    }
+    
+    include DIGI_VCARD_PLUGIN_PATH . 'templates/single-vcard.php';
+}
+
+/**
+ * Shortcode for displaying vCard
+ */
+function vcard_shortcode($atts) {
+    $atts = shortcode_atts(array(
+        'id' => get_the_ID(),
+    ), $atts);
+    
+    ob_start();
+    display_vcard($atts['id']);
+    return ob_get_clean();
+}
+add_shortcode('vcard', 'vcard_shortcode');
---
+++ wordpress/wp-content/plugins/digi-vcard/templates/single-vcard.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Single vCard Template
+ */
+
+$post_id = $post_id ?? get_the_ID();
+$post = get_post($post_id);
+
+// Get vCard data
+$job_title = get_vcard_meta($post_id, 'job_title');
+$company = get_vcard_meta($post_id, 'company');
+$email = get_vcard_meta($post_id, 'email');
+$phone = get_vcard_meta($post_id, 'phone');
+$office_phone = get_vcard_meta($post_id, 'office_phone');
+$address = get_vcard_meta($post_id, 'address');
+?>
+
+<div class="vcard-container">
+    <!-- vCard Header -->
+    <div class="vcard-header">
+        <?php if (has_post_thumbnail($post_id)) : ?>
+            <div class="vcard-profile-image">
+                <?php echo get_the_post_thumbnail($post_id, 'medium'); ?>
+            </div>
+        <?php endif; ?>
+        
+        <h1 class="vcard-name"><?php echo esc_html(get_the_title($post_id)); ?></h1>
+        
+        <?php if ($job_title) : ?>
+            <p class="vcard-job-title"><?php echo esc_html($job_title); ?></p>
+        <?php endif; ?>
+        
+        <?php if ($company) : ?>
+            <p class="vcard-company"><?php echo esc_html($company); ?></p>
+        <?php endif; ?>
+    </div>
+
+    <div class="vcard-content">
+        <!-- About Section -->
+        <?php if ($post->post_content) : ?>
+            <div class="vcard-section">
+                <h2 class="vcard-section-title">About</h2>
+                <div class="vcard-bio">
+                    <?php echo wp_kses_post(wpautop($post->post_content)); ?>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <!-- Contact Information -->
+        <?php if ($email || $phone || $office_phone || $address) : ?>
+            <div class="vcard-section">
+                <h2 class="vcard-section-title">Contact Information</h2>
+                <div class="vcard-contact-grid">
+                    
+                    <?php if ($email) : ?>
+                        <div class="vcard-contact-item">
+                            <div class="vcard-contact-icon">📧</div>
+                            <div class="vcard-contact-details">
+                                <h4>Email</h4>
+                                <a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if ($phone) : ?>
+                        <div class="vcard-contact-item">
+                            <div class="vcard-contact-icon">📱</div>
+                            <div class="vcard-contact-details">
+                                <h4>Phone</h4>
+                                <a href="tel:<?php echo esc_attr($phone); ?>"><?php echo esc_html($phone); ?></a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if ($office_phone) : ?>
+                        <div class="vcard-contact-item">
+                            <div class="vcard-contact-icon">🏢</div>
+                            <div class="vcard-contact-details">
+                                <h4>Office</h4>
+                                <a href="tel:<?php echo esc_attr($office_phone); ?>"><?php echo esc_html($office_phone); ?></a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if ($address) : ?>
+                        <div class="vcard-contact-item">
+                            <div class="vcard-contact-icon">📍</div>
+                            <div class="vcard-contact-details">
+                                <h4>Address</h4>
+                                <div><?php echo wp_kses_post(nl2br($address)); ?></div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <!-- Actions -->
+    <div class="vcard-actions">
+        <button class="vcard-btn" onclick="downloadVCard()">
+            📱 Add to Contacts
+        </button>
+        <button class="vcard-btn secondary" onclick="shareVCard()">
+            📤 Share vCard
+        </button>
+    </div>
+</div>
+
+<script>
+// Initialize vCard data
+document.addEventListener('DOMContentLoaded', function() {
+    initVCard({
+        name: <?php echo json_encode(get_the_title($post_id)); ?>,
+        title: <?php echo json_encode($job_title); ?>,
+        company: <?php echo json_encode($company); ?>,
+        email: <?php echo json_encode($email); ?>,
+        phone: <?php echo json_encode($phone); ?>,
+        address: <?php echo json_encode(str_replace(array("\r\n", "\n", "\r"), ";", $address)); ?>,
+        filename: <?php echo json_encode(sanitize_file_name(get_the_title($post_id)) . '.vcf'); ?>
+    });
+});
+</script>
