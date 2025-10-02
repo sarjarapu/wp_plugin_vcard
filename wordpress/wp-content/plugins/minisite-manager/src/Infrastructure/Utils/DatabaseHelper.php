9c50463c30b2cabcb24734c719a7db46d4322fcc
Shyam <shyam@nimbleaiservices.com>
2025-10-01 20:32:42 -0500

    lint: major classes being changed with dbhelper instead of wpdb

--- wordpress/wp-content/plugins/minisite-manager/src/Infrastructure/Utils/DatabaseHelper.php
+++ wordpress/wp-content/plugins/minisite-manager/src/Infrastructure/Utils/DatabaseHelper.php
@@ -10,56 +10,56 @@ final class DatabaseHelper
     public static function get_var(string $sql, array $params = []): mixed
     {
         global $wpdb;
-        
+
         if (!empty($params)) {
             $sql = $wpdb->prepare($sql, ...$params);
         }
-        
+
         return $wpdb->get_var($sql);
     }
-    
+
     /**
      * Same as $wpdb->get_row() - drop-in replacement
      */
     public static function get_row(string $sql, array $params = []): mixed
     {
         global $wpdb;
-        
+
         if (!empty($params)) {
             $sql = $wpdb->prepare($sql, ...$params);
         }
-        
+
         return $wpdb->get_row($sql, ARRAY_A);
     }
-    
+
     /**
      * Same as $wpdb->get_results() - drop-in replacement
      */
     public static function get_results(string $sql, array $params = []): mixed
     {
         global $wpdb;
-        
+
         if (!empty($params)) {
             $sql = $wpdb->prepare($sql, ...$params);
         }
-        
+
         return $wpdb->get_results($sql, ARRAY_A);
     }
-    
+
     /**
      * Same as $wpdb->query() - drop-in replacement
      */
     public static function query(string $sql, array $params = []): mixed
     {
         global $wpdb;
-        
+
         if (!empty($params)) {
             $sql = $wpdb->prepare($sql, ...$params);
         }
-        
+
         return $wpdb->query($sql);
     }
-    
+
     /**
      * Same as $wpdb->insert() - drop-in replacement
      */
@@ -68,7 +68,7 @@ final class DatabaseHelper
         global $wpdb;
         return $wpdb->insert($table, $data, $format);
     }
-    
+
     /**
      * Same as $wpdb->update() - drop-in replacement
      */
@@ -77,7 +77,7 @@ final class DatabaseHelper
         global $wpdb;
         return $wpdb->update($table, $data, $where, $format, $where_format);
     }
-    
+
     /**
      * Same as $wpdb->delete() - drop-in replacement
      */
@@ -86,7 +86,7 @@ final class DatabaseHelper
         global $wpdb;
         return $wpdb->delete($table, $where, $where_format);
     }
-    
+
     /**
      * Same as $wpdb->insert_id - drop-in replacement
      */
@@ -96,4 +96,3 @@ final class DatabaseHelper
         return (int) $wpdb->insert_id;
     }
 }
-
